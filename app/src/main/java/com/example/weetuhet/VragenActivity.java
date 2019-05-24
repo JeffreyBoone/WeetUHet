2dab152b2e1d2a692fb1be6626187f1bb305a7b9
Jeffrey Boone <302948965@student.rocmondriaan.nl>
2019-05-24 12:53:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/weetuhet/VragenActivity.java
@@ -0,0 +1,13 @@
+package com.example.weetuhet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class VragenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
